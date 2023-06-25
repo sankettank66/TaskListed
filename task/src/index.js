0222884c6824eba84c5c117fe4acaa7f634bca88
Sanket Tank <81406103+sankettank66@users.noreply.github.com>
2023-06-25 23:44:32 +0530

    Added Warning

--- task/src/index.js
+++ task/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import DashboardUi from './DashboardUi';
+import App from './App';
+// import DashboardUi from './DashboardUi';
+// Add only if you want to render the component Dashboard
+// like don't add both components in files, it will change CSS for the component
 import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <DashboardUi />
+     <App /> 
+  {/*<DashboardUi />*/}
   </React.StrictMode>
 );
 
